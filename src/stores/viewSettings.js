92c846140f65609005ef7825f3ce215cf4bc5a2a
netcam22 <annettelesage@btinternet.com>
2024-01-29 08:12:41 +0000

    fix: image url

--- src/stores/viewSettings.js
+++ src/stores/viewSettings.js
@@ -1,12 +1,16 @@
 import { defineStore } from 'pinia';
+import fakelandiaLogo from "./../assets/fakelandiaLogo.png";
+import HHHlogo from "./../assets/logoHHH.png";
+import iteduLogo from "./../assets/itedulogo.png";
+import smallLogo from "./../assets/logoSmall.png";
 
 export const useViewSettingsStore = defineStore("viewSettings", {
   state: () => ({
     imageSources: {
-      home: "fakelandiaLogo.png",
-      about: "logoHHH.png",
-      contact: "itedulogo.png",
-      items: "logoSmall.png",
+      home: fakelandiaLogo,
+      about: HHHlogo,
+      contact: iteduLogo,
+      items: smallLogo,
       },
   }
   ),
